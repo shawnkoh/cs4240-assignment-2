f2314fa239c6747b66cb112338f5271d2fb36e2f
Shawn Koh <shawn@shawnkoh.sg>
2022-02-21 11:05:19 +0800

    Move folders

--- Assets/StateMachine/CurrentValueSubject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace StateMachine {
-    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
-    public class CurrentValueSubject : ScriptableObject {
-    }
-}
